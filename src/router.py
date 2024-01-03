a4349c24a48a2c97d767390b74353d8f788c18e2
leins275 <leins275@gmail.com>
2024-01-03 18:39:42 +0300

    Added address pattern query parameter to history search api request

--- src/router.py
+++ src/router.py
@@ -1,6 +1,6 @@
 from loguru import logger
 from fastapi import APIRouter, Depends
-from sqlalchemy import select
+from sqlalchemy import select, or_
 from src.database import get_db
 from src.database.db import Database
 from src.database.models import SearchLog
@@ -22,8 +22,12 @@ async def search_person(name="", address="", ps: PeopleSearch = Depends(get_sear
 
 
 @main_router.get("/history")
-async def get_previous_search_results(pattern, db: Database = Depends(get_db)):
+async def get_previous_search_results(name_pattern="", address_pattern="", db: Database = Depends(get_db)):
     '''This function returns all search requests logged at search_log db table. '''
-    query = select(SearchLog).where(SearchLog.name_search_pattern.like(f'%{pattern}%'))
+    query = select(SearchLog).where(
+        or_(
+            SearchLog.name_search_pattern.like(f'%{name_pattern}%'),
+            SearchLog.address_search_pattern.like(f'%{address_pattern}%'),
+            ))
     res = db.sql_query(query=query, single=False)
     return {"message": "success", "data": res} 
